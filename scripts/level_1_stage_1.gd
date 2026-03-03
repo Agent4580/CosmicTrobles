8a615a8100aa54d6ce4e15a737480dc30a8154ae
Agent4580 <sashapetrov89198258034@gmail.com>
2026-03-03 11:29:33 +0400

    Создание этапа 3 уровня 1. Устранение неполадок на других этапах

--- scripts/level_1_stage_1.gd
+++ scripts/level_1_stage_1.gd
@@ -4,4 +4,4 @@ extends Node2D
 
 func _on_finish_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+		get_tree().change_scene_to_file("res://scenes/level_1_stage_2.tscn")
--- scripts/level_menu.gd
+++ scripts/level_menu.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _on_level_1_button_down() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_1_stage_1.tscn")
