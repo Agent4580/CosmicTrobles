715709f3983069994fb189f4c55038d6b7b90cfd
Agent4580 <sashapetrov89198258034@gmail.com>
2026-03-05 10:22:19 +0400

    Добавление текста завершения уровня и исправление багов

--- scripts/level_1_stage_1.gd
+++ scripts/level_1_stage_1.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-
+func _ready() -> void:
+	pass
 
 func _on_finish_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
--- scripts/level_1_stage_3.gd
+++ scripts/level_1_stage_3.gd
@@ -3,4 +3,21 @@ extends Node2D
 
 func _on_finish_body_entered(body: Node2D) -> void:
 	if body.is_in_group('Player'):
-		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		get_tree().set_pause(true)
+		get_node('Finish_menu').set_visible(true)
+
+
+func _on_exit_button_up() -> void:
+	get_tree().set_pause(false)
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	
+
+func _on_continue_button_down() -> void:
+	get_tree().set_pause(false)
+	get_tree().change_scene_to_file('res://level_2_temporary.tscn')
+	
+
+func _on_reset_button_down() -> void:
+	get_tree().set_pause(false)
+	get_tree().change_scene_to_file('res://scenes/level_1_stage_1.tscn')
+	
